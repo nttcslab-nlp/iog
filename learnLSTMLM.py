d0a9b5bca13bd65a2b61b4183232dbc3f2cb15f2
Sho Takase <takase@tb100.cslab.kecl.ntt.co.jp>
2017-08-28 10:53:33 +0900

    Add reimplementations on IJCNLP 2017

---
+++ learnLSTMLM.py
@@ -0,0 +1,236 @@
+#coding: utf-8
+
+#learn LSTM language model introduced by Zaremba et al., https://arxiv.org/abs/1409.2329
+#input: train and valid corpus (replaced to npz file by utils.py), vocab file
+#output: trained language model
+
+#use chainer version 1.24.0
+#This program cannot run on chainer v2
+
+
+import sys
+import numpy as np
+import numpy as xp
+import time
+import copy
+import argparse
+from six.moves import cPickle as pickle
+from six.moves import range as range
+import chainer
+import chainer.functions as F
+import chainer.optimizers as O
+import chainer.links as L
+from chainer import cuda
+import chainer.serializers as S
+import chainer.initializers as I
+
+
+class SettingData:
+    def __init__(self, learnedModel):
+        self.vocab = learnedModel.vocab
+        self.modelType = learnedModel.modelType
+        self.dim = learnedModel.dim
+        self.layerNum = learnedModel.layerNum
+        self.depth = learnedModel.depth
+
+
+class LSTMLM:
+    def __init__(self, dim, vocab, layerNum, depth=1, modelType='LSTMLM'):
+        self.dim = dim
+        self.vocab = vocab
+        self.layerNum = layerNum
+        self.modelType = modelType
+        self.depth = depth
+
+
+    def make_network(self, scale):
+        dim = self.dim
+        self.lmNet = chainer.Chain()
+        self.lmNet.add_link('Embed', L.EmbedID(len(self.vocab), dim, initialW=I.Uniform(scale)))
+        for j in range(self.layerNum):
+            self.lmNet.add_link('LSTM%s'%(j), L.LSTM(dim, dim, lateral_init=I.Uniform(scale), upward_init=I.Uniform(scale)))
+        self.lmNet.add_link('Output', L.Linear(dim, len(self.vocab), initialW=I.Uniform(scale)))
+
+
+    def compute_forward(self, wordIndex, prevHidden, dropout=0.0):
+        #input: input word indices (chainer Variable)
+        #prev hidden: previous hidden state
+        train = dropout > 0.0
+        embed = self.lmNet.Embed(wordIndex)
+        h = F.dropout(embed, ratio=dropout, train=train)
+        for j in range(self.layerNum):
+            lstm = self.lmNet.__getitem__('LSTM%s'%(j))
+            if prevHidden is None:
+                lstm.reset_state()
+            lstm(h)
+            h = F.dropout(lstm.h, ratio=dropout, train=train)
+        y = self.lmNet.Output(h)
+        return y, h
+
+
+def read_vocab(vocabFile):
+    d = {}
+    for line in open(vocabFile):
+        line = line.strip()
+        d[line] = len(d)
+    return d
+
+
+def make_batch(data, batchsize, step):
+    #data is chainer Variable representing word indices
+    dataLength = len(data)
+    batchLength = dataLength // batchsize
+    data4batch = F.reshape(data[0 : batchsize * batchLength], (batchsize, batchLength))
+    for i in range(0, batchLength - 1, step):
+        current_words = data4batch[:, i : min(i+step, batchLength - 1)]
+        next_words = data4batch[:, i+1 : min(i+1+step, batchLength)]
+        yield current_words, next_words
+
+
+def train_with_batch(current_words, next_words, lmWithRNN, args, prevHidden=None):
+    #current words and next words are index matrix (batch * step)
+    loss = 0
+    for index in range(current_words.shape[1]):
+        wordIndex = current_words[:, index]
+        y, prevHidden = lmWithRNN.compute_forward(wordIndex, prevHidden, args.dropout)
+        loss += F.softmax_cross_entropy(y, next_words[:, index])
+    return loss, prevHidden
+
+
+def valid(validData, lmWithRNN):
+    #valid data is chainer variable
+    prevHidden = None
+    loss = 0
+    for i in range(len(validData) - 1):
+        y, prevHidden = lmWithRNN.compute_forward(validData[i : i+1], prevHidden)
+        loss += F.softmax_cross_entropy(y, validData[i+1 : i+2])
+    loss = float(F.sum(loss).data) / (len(validData) - 1)
+    perp = np.exp(loss)
+    return loss, perp
+
+
+def valid_with_batch(validData, lmWithRNN):
+    batchsize = 50
+    prevHidden = None
+    totalins = len(validData) - 1
+    loss = 0
+    for current_words, next_words in make_batch(validData, batchsize, 100000):
+        for index in range(current_words.shape[1]):
+            wordIndex = current_words[:, index]
+            y, prevHidden = lmWithRNN.compute_forward(wordIndex, prevHidden)
+            loss += F.softmax_cross_entropy(y, next_words[:, index]) * batchsize
+    loss = float(F.sum(loss).data) / totalins
+    perp = np.exp(loss)
+    return loss, perp
+
+
+def train(lmWithRNN, args, trainData, validData):
+    #trainData and validData is chainer Variable
+    if args.gpu >= 0:
+        lmWithRNN.lmNet.to_gpu()
+    if args.WT:
+        lmWithRNN.lmNet.Output.W.data = lmWithRNN.lmNet.Embed.W.data
+    opt = O.SGD(args.lr)
+    opt.setup(lmWithRNN.lmNet)
+    opt.add_hook(chainer.optimizer.GradientClipping(args.maxGrad))
+    prevvalidperp = np.inf
+    prevModel = None
+    for epoch in range(args.epoch):
+        epochStart = time.time()
+        lr_decay = args.decay ** max(epoch + 1 - args.decayEpoch, 0.0)
+        opt.lr = args.lr * lr_decay
+        sys.stdout.write('Learning rate: %.6f\n'%(opt.lr))
+        totalloss = 0
+        finishnum = 0
+        prevHidden = None
+        for current_words, next_words in make_batch(trainData, args.batch, args.step):
+            lmWithRNN.lmNet.cleargrads()
+            loss, prevHidden = train_with_batch(current_words, next_words, lmWithRNN, args, prevHidden)
+            loss.backward()
+            loss.unchain_backward()
+            opt.update()
+            totalloss += float(F.sum(loss).data) * current_words.shape[0]
+            finishnum += current_words.shape[0] * current_words.shape[1]
+            sys.stderr.write('\r Finished %s'%finishnum)
+        sys.stderr.write('\n')
+        epochEnd = time.time()
+        if args.valid_with_batch:
+            validloss, validperp = valid_with_batch(validData, lmWithRNN)
+        else:
+            validloss, validperp = valid(validData, lmWithRNN)
+        sys.stdout.write('Train time is %s\tValid time is %s\n'%(epochEnd - epochStart, time.time() - epochEnd))
+        sys.stdout.write('Epoch: %s\tTrain loss: %.6f\tValid loss: %.6f\tValid perplexity: %.6f\n'%(epoch, totalloss / finishnum, validloss, validperp))
+        sys.stdout.flush()
+        if prevvalidperp < validperp:
+            lmOutputFile = args.output + '.epoch%s'%(epoch) + '.bin'
+            S.save_npz(lmOutputFile, prevModel)
+        prevModel = copy.deepcopy(lmWithRNN.lmNet).to_cpu()
+        prevvalidperp = validperp
+    lmOutputFile = args.output + '.epoch%s_fin'%(epoch+1) + '.bin'
+    S.save_npz(lmOutputFile, prevModel)
+
+
+def main(args):
+    vocab = read_vocab(args.vocab)
+    trainData = chainer.Variable(xp.array(np.load(args.train)['arr_0'], dtype=np.int32))
+    validData = chainer.Variable(xp.array(np.load(args.valid)['arr_0'], dtype=np.int32), volatile='on')
+    lmWithRNN = LSTMLM(args.dim, vocab, args.layerNum)
+    lmWithRNN.make_network(args.scale)
+    settingData = SettingData(lmWithRNN)
+    outputFile = open(args.output + '.setting', 'w')
+    pickle.dump(settingData, outputFile)
+    outputFile.close()
+    train(lmWithRNN, args, trainData, validData)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    #default hyper parameter is the same as medium model implemented by tensorflow (https://github.com/tensorflow/models/blob/master/tutorials/rnn/ptb/ptb_word_lm.py)
+    parser.add_argument('-v', '--vocab', dest='vocab', default='',
+        help='specify the name of file representing vocabulary')
+    parser.add_argument('--train', dest='train', default='',
+        help='specify training file')
+    parser.add_argument('--valid', dest='valid', default='',
+        help='specify validation data')
+    parser.add_argument('--output', dest='output', default='',
+        help='specify output file name')
+    parser.add_argument('--scale', dest='scale', default=0.05, type=float,
+        help='specify the scale value for initialization')
+    parser.add_argument('--lr', dest='lr', default=1.0, type=float,
+        help='specify the initial learining rate')
+    parser.add_argument('--maxGrad', dest='maxGrad', default=5, type=float,
+        help='specify the max gradient norm')
+    parser.add_argument('--layer', dest='layerNum', default=2, type=int,
+        help='specify the number of RNN layers')
+    parser.add_argument('--step', dest='step', default=35, type=int,
+        help='specify the number of steps to update parameters')
+    parser.add_argument('-d', '--dim', dest='dim', default=650, type=int,
+        help='specify the number of dimensions')
+    parser.add_argument('--decayEpoch', dest='decayEpoch', default=6, type=int,
+        help='specify the epoch to keep initial learning rate')
+    parser.add_argument('-e', '--epoch', dest='epoch', default=39, type=int,
+        help='specify the number of epoch')
+    parser.add_argument('--dropout', dest='dropout', default=0.5, type=float,
+        help='specify the dropout rate')
+    parser.add_argument('--decay', dest='decay', default=0.833333, type=float,
+        help='specify the value for epoch decay')
+    parser.add_argument('-b', '--batch', dest='batch', default=20, type=int,
+        help='specify the size of batch')
+    parser.add_argument('-g', '--gpu', dest='gpu', default=-1, type=int,
+        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--seed', dest='seed', default=0, type=int,
+        help='specify the number of seed')
+    parser.add_argument('--WT', dest='WT', default=False, action='store_true',
+        help='whether to share embedding matrix with output or not')
+    parser.add_argument('--valid_with_batch', dest='valid_with_batch', default=False, action='store_true',
+        help='whether valid with batch or not')
+    args = parser.parse_args()
+    if args.gpu >= 0:
+        cuda.check_cuda_available()
+        cuda.get_device(args.gpu).use()
+    xp = cuda.cupy if args.gpu >= 0 else np
+    np.random.seed(args.seed)
+    xp.random.seed(args.seed)
+    main(args)
+
+
---
+++ testIOG.py
@@ -0,0 +1,123 @@
+#coding: utf-8
+
+#test trained input-to-output gate model
+#input: test corpus (replaced to npz file by utils.py), IOG setting file, and IOG model file
+#output: loss value and perplexity
+
+#use chainer version 1.24.0
+
+import sys
+from six.moves import cPickle as pickle
+from six.moves import range as range
+import numpy as np
+import numpy as xp
+import argparse
+import chainer
+from chainer import cuda
+import chainer.serializers as S
+import chainer.functions as F
+
+from learnLSTMLM import SettingData
+from learnLSTMLM import LSTMLM
+from learnRecurrentHighwayNet import RHN
+from learnIOG import Setting4Gate
+from learnIOG import GateModel
+
+
+def valid(validData, lmWithRNN, gateModel, prev=None):
+    loss = 0
+    for i in xrange(len(validData) - 1):
+        y, prev = lmWithRNN.compute_forward(validData[i : i+1], prev)
+        gate = gateModel.compute_gate(validData[i : i+1])
+        loss += F.softmax_cross_entropy(y * gate, validData[i+1 : i+2])
+    loss = float(F.sum(loss).data) / (len(validData) - 1)
+    perp = np.exp(loss)
+    return loss, perp
+
+
+def valid_with_cachemodel(validData, lmWithRNN, gateModel, cachesize, prev):
+    #computing neural cache model, with lambda 0.2, theta 0.3
+    lam = 0.2
+    theta = 0.3
+    loss = 0
+    cache = []
+    for i in xrange(len(validData) - 1):
+        sys.stderr.write('\r%s'%i)
+        if i != 0:
+            z = xp.zeros((min(cachesize, i), len(lmWithRNN.vocab)), dtype=np.float32)
+            z[xp.arange(min(cachesize, i)), validData.data[max(1, i - cachesize + 1) : i+1]] = 1
+            one_hot = chainer.Variable(z, volatile='on')
+        y, prev = lmWithRNN.compute_forward(validData[i : i+1], prev)
+        if type(prev) is list:
+            h = prev[-1]
+        else:
+            h = prev
+        if i != 0:
+            c = F.vstack(cache)
+            cdot = F.exp(theta * F.sum(c * F.broadcast_to(h, c.shape), axis=1, keepdims=True))
+            cdotm = F.broadcast_to(cdot, one_hot.shape)
+            cdotsum = F.sum(cdotm * one_hot, axis=0, keepdims=True)
+            cacheprob = cdotsum / F.sum(cdotsum).data
+        cache.append(h)
+        cache = cache[-cachesize:]
+        gate = gateModel.compute_gate(validData[i : i+1])
+        lingprob = F.softmax(y * gate)
+        if i == 0:
+            p = lingprob
+        else:
+            p = lam * F.reshape(cacheprob, (1, len(lmWithRNN.vocab))) + (1 - lam) * lingprob
+        logp = -F.log(p)
+        loss += logp[0, validData[i+1 : i+2].data[0]]
+    loss = float(F.sum(loss).data) / (len(validData) - 1)
+    perp = np.exp(loss)
+    sys.stderr.write('/n')
+    return loss, perp
+
+
+def main(args):
+    modelData = pickle.load(open(args.setting))
+    gateModel = GateModel(modelData.vocab, modelData.dim)
+    gateModel.make_network(1.0)
+    S.load_npz(args.model, gateModel.gateModel)
+    lmModelData = pickle.load(open(modelData.settingFile))
+    if 'RHN' in lmModelData.modelType:
+        lmWithRNN = RHN(lmModelData.dim, lmModelData.vocab, lmModelData.depth)
+    else:
+        lmWithRNN = LSTMLM(lmModelData.dim, lmModelData.vocab, lmModelData.layerNum)
+    lmWithRNN.make_network(1.0)
+    S.load_npz(modelData.modelFile, lmWithRNN.lmNet)
+    if args.gpu >= 0:
+        lmWithRNN.lmNet.to_gpu()
+        gateModel.gateModel.to_gpu()
+    testData = chainer.Variable(xp.array(np.load(args.test)['arr_0'], dtype=np.int32), volatile='on')
+    if 'RHN' in lmModelData.modelType:
+        prev = [chainer.Variable(xp.zeros((1, lmModelData.dim)).astype(np.float32), volatile='on') for _ in range(lmModelData.layerNum)]
+    else:
+        prev = None
+    if args.cachesize > 0:
+        testloss, testperp = valid_with_cachemodel(testData, lmWithRNN, gateModel, args.cachesize, prev)
+    else:
+        testloss, testperp = valid(testData, lmWithRNN, gateModel, prev)
+    print 'Loss: %.6f\tPerplexity: %.6f'%(testloss, testperp)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--test', dest='test', default='',
+       help='specify the test data')
+    parser.add_argument('-g', '--gpu', dest='gpu', default=-1, type=int,
+        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--cachesize', dest='cachesize', default=0, type=int,
+        help='specify the size of cache')
+    parser.add_argument('-s', '--setting', dest='setting', default='', required=True,
+        help='specify the setting file of trained IOG (including the path of the used language model')
+    parser.add_argument('-m', '--model', dest='model', default='', required=True,
+        help='specify the name of trained IOG')
+    args = parser.parse_args()
+    if args.gpu >= 0:
+        cuda.check_cuda_available()
+        cuda.get_device(args.gpu).use()
+    xp = cuda.cupy if args.gpu >= 0 else np
+    main(args)
+
+
