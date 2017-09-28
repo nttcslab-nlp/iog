df90c71f91bed424aafee8cb72d5ef422f1090f6
Sosuke Kobayashi <sosk@preferred.jp>
2017-09-28 22:17:50 +0900

    fix downloader

--- preparedata.sh
+++ preparedata.sh
@@ -6,13 +6,13 @@ echo 'Downloading Penn Treebank dataset'
 
 mkdir -p ptb
 cd ptb
-wget --quiet --continue https://github.com/wojzaremba/lstm/blob/master/data/ptb.train.txt
-wget --quiet --continue https://github.com/wojzaremba/lstm/blob/master/data/ptb.valid.txt
-wget --quiet --continue https://github.com/wojzaremba/lstm/blob/master/data/ptb.test.txt
+wget --quiet --continue https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.train.txt
+wget --quiet --continue https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.valid.txt
+wget --quiet --continue https://raw.githubusercontent.com/wojzaremba/lstm/master/data/ptb.test.txt
 
 echo 'Making .npz file and vocabulary for Penn Treebank dataset'
 
-cd ../..
+cd ..
 python ../utils.py -i ptb -o ptb -p ptb > ptb/ptb.train.vocab
 
 
